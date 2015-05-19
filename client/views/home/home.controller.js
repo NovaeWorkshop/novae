909010369c0dee2b5f9a50bde2fbd2c7b565d90c
Christophe Feijoo <kubekhrm@gmail.com>
2015-05-19 21:46:42 +0200

    Parallax effect

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -2,8 +2,25 @@
 
 angular.module('novae')
   .controller('HomeCtrl', function () {
-
     var vm = this;
+    var galaxy = document.getElementsByClassName('galaxy-layer')[0];
+
+    var request = null;
+
+    angular.element(window).bind('scroll', function (e) {
+
+      if (document.body.scrollTop > parseInt(window.getComputedStyle(galaxy).getPropertyValue('height')))
+        return;
+
+      if (request)
+        window.cancelAnimationFrame(request);
+
+      request = window.requestAnimationFrame(function () {
+        galaxy.style.webkitTransform = 'translate3d(0, ' + parseInt(document.body.scrollTop * 0.4) + 'px, 0)';
+        request = null;
+      });
+
+    });
 
     angular.extend(vm, {
       name: 'HomeCtrl'
