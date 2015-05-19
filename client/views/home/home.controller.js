5f4120f4542a8813ce8b9f7e7f093ea4bdcf5e11
Christophe Feijoo <kubekhrm@gmail.com>
2015-05-19 21:59:18 +0200

    Blur on scroll

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -4,12 +4,14 @@ angular.module('novae')
   .controller('HomeCtrl', function () {
     var vm = this;
     var galaxy = document.getElementsByClassName('galaxy-layer')[0];
+    var galaxyBlurred = document.getElementsByClassName('galaxy-blurred-layer')[0];
 
     var request = null;
 
     angular.element(window).bind('scroll', function (e) {
+      var galaxyHeight = parseInt(window.getComputedStyle(galaxy).getPropertyValue('height'));
 
-      if (document.body.scrollTop > parseInt(window.getComputedStyle(galaxy).getPropertyValue('height')))
+      if (document.body.scrollTop > galaxyHeight)
         return;
 
       if (request)
@@ -17,6 +19,9 @@ angular.module('novae')
 
       request = window.requestAnimationFrame(function () {
         galaxy.style.webkitTransform = 'translate3d(0, ' + parseInt(document.body.scrollTop * 0.4) + 'px, 0)';
+        galaxyBlurred.style.webkitTransform = 'translate3d(0, ' + parseInt(document.body.scrollTop * 0.4) + 'px, 0)';
+        galaxy.style.opacity = (galaxyHeight * 2 / 3 - document.body.scrollTop) / (galaxyHeight * 2 / 3);
+
         request = null;
       });
 
